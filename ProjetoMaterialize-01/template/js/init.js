c0f6c3b8f7415ee516d99fd73271732f04739f81
rolvander <fabiorolvander@gmail.com>
2021-04-18 21:04:42 -0300

    Adicionado projeto de template com Materialize

---
+++ ProjetoMaterialize-01/template/js/init.js
@@ -0,0 +1,13 @@
+(function($){
+  $(function(){
+
+	$(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
+
+   $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
